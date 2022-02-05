6242ddc6741a7182f98173572cffd6547ba985c0
kon2300 <yyaamm.2300@gmail.com>
2022-02-05 18:50:21 +0900

    :technologist: Set up for vite

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -1,1 +1,30 @@
-require('./bootstrap');
+import "./bootstrap";
+
+import { createApp, h } from "vue";
+import { createInertiaApp } from "@inertiajs/inertia-vue3";
+import { InertiaProgress } from "@inertiajs/progress";
+import "../css/app.css";
+
+const appName =
+    window.document.getElementsByTagName("title")[0]?.innerText || "Laravel";
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: async (name) => {
+        if (import.meta.env.DEV) {
+            return await import(`./Pages/${name}.vue`);
+        } else {
+            let pages = import.meta.glob("./Pages/**/*.vue");
+            const importPage = pages[`./Pages/${name}.vue`];
+            return importPage().then(module);
+        }
+    },
+    setup({ el, app, props, plugin }) {
+        return createApp({ render: () => h(app, props) })
+            .use(plugin)
+            .mixin({ methods: { route } })
+            .mount(el);
+    },
+});
+
+InertiaProgress.init({ color: "#4B5563" });
